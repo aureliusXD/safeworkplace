45463330758fed343194f044f2869850848755f1
aurelius <aurelius@MacBook-Pro-de-Eddy.local>
2023-10-10 10:13:03 +0200

    Premier commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var mnnav=document.getElementById('drpmn')
+var drp=document.getElementById('drpd')
+
+drp.addEventListener('click',function() {
+    mnnav.style.display="block"
+})
---
+++ login.js
@@ -0,0 +1,8 @@
+var sb=document.getElementById("sub")
+var nt=document.getElementById("notif")
+
+sb.addEventListener("click",function () {
+    nt.style.display="inline"
+
+    
+})
